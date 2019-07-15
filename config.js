eb8be59a4470221a78153aa7731ab791afdbbfea
David Francis <dfran1991@gmail.com>
2019-07-14 22:01:52 -0700

    Use gatsby theme for landing page

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'DF Productions', // <title>
+  manifestName: 'DF-Productions',
+  manifestShortName: 'Productions', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
+  // social
+
+  authorName: 'DF Productions',
+  authorHeading: 'Photography - Videography - Film',
+  authorSocialLinks: [
+    // {
+    //   icon: 'fa-github',
+    //   name: 'Github',
+    //   url: 'https://github.com/anubhavsrivastava',
+    // },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
+    // {
+    //   icon: 'fa-facebook',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/theanubhav',
+    // },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:info@df.productions',
+    },
+  ],
+};
